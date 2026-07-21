5ac466c0cc22b2ebf2b1a23465fe79ebaa514971
Jadakai78 <blazing0478@gmail.com>
2026-07-21 06:09:11 -0500

    Update tak_scanner_v4.py

--- tak_scanner_v4.py
+++ tak_scanner_v4.py
@@ -114,7 +114,7 @@ class TakScannerV4:
 
         logger.info("Oracle building contexts for %s active pairs", len(active))
         for pair in active:
-            regime = self.classifier.classify(pair)
+            regime = self.classifier.classify(pair, ohlc_df, fg_score)
             regime_map[pair] = regime
             contexts.append(
                 PairContext(
