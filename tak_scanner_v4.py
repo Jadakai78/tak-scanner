07a9516560a3bc12c931719f0f4f1b2a06d2a736
Jadakai78 <blazing0478@gmail.com>
2026-07-21 07:59:31 -0500

    Update tak_scanner_v4.py

--- tak_scanner_v4.py
+++ tak_scanner_v4.py
@@ -6,6 +6,7 @@ from datetime import datetime, timedelta, timezone
 from pathlib import Path
 from typing import Any, Dict, List, Optional
 
+import pandas as pd
 import requests
 
 from convictionscorer import ConvictionScorer
