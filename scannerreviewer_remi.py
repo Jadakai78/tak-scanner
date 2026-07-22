b8f65c96b16844db3492cdd5f622c01a7958c716
Jadakai78 <blazing0478@gmail.com>
2026-07-22 06:44:33 -0500

    Refactor RemiReviewer class and add imports

--- scannerreviewer_remi.py
+++ scannerreviewer_remi.py
@@ -4,6 +4,14 @@ from typing import List
 
 from scannermodels import CandidateSignal, ReviewResult
 
+from scannercandidate_factory import build_candidate
+
+from scannermodels import CandidateSignal, PairContext, SpecialistObservation
+
+from scannercouncil import ScannerCouncil
+
+from scannerreviewer_remi import RemiReviewer
+
 
 class RemiReviewer:
     def __init__(
