a16600a4194b6131b805a5a357ebf9ff78ab6da6
Jadakai78 <blazing0478@gmail.com>
2026-07-22 06:53:00 -0500

    Refactor candidate building functions

--- scannercandidate_factory.py
+++ scannercandidate_factory.py
@@ -4,11 +4,6 @@ from typing import Any, Dict
 from uuid import uuid4
 
 from scannermodels import CandidateSignal, SpecialistObservation
-from scannercandidate_factory import build_candidate
-from scannermodels import CandidateSignal, PairContext, SpecialistObservation
-from scannercouncil import ScannerCouncil
-from scannerreviewer_remi import RemiReviewer
-
 
 
 
